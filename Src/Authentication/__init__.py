4652eeead5ced198e430e93da562bce1a174f1f7
mahakweb <mahaksaxena0353@gmail.com>
2025-05-08 23:57:01 +0530

    first commit

---
+++ Src/Authentication/__init__.py
@@ -0,0 +1,23 @@
+import json
+import os
+USER_FILE = 'users.json'
+
+def init_user_file():
+    if not os.path.exists(USER_FILE):
+        with open(USER_FILE, 'w') as f:
+            json.dump([], f)
+
+def load_users():
+    try:
+        with open(USER_FILE, 'r') as f:
+            data = json.load(f)
+            if isinstance(data, list):
+                return data
+            else:
+                return []
+    except json.JSONDecodeError:
+        return []
+
+def save_users(users):
+    with open(USER_FILE, 'w') as f:
+        json.dump(users, f, indent=4)
---
+++ Src/Authentication/sign_in.py
@@ -0,0 +1,19 @@
+from sign_in import sign_in
+
+def main():
+    while True:
+        print("\n1. Sign In\n2. Sign Up\n3. Exit")
+        choice = input("Choose an option: ")
+
+        if choice == '1':
+            sign_in()
+        elif choice == '2':
+            print("Sign Up option not yet implemented.")  
+        elif choice == '3':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid option. Try again.")
+
+if __name__ == "__main__":
+    main()
